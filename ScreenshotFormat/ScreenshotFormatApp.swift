76b20d7cc70f7bb2e712d7210dac872fa035a367
Iván Cabezón <nscabezon@gmail.com>
2026-09-19 12:16:22 +0200

    Initial Commit

---
+++ ScreenshotFormat/ScreenshotFormatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotFormatApp.swift
+//  ScreenshotFormat
+//
+//  Created by nscabezon on 19/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotFormatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
